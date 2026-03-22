fcbb090a39cc603fb90e58d6041da2662f009c6c
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-22 13:23:40 +0530

    refactor: clean up mail service by removing transporter verification logs

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookie());
 
-app.use(express.static(path.join(__dirname, "../public")))// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "../public")))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
